b01276b19812e0a57fd2b63b720d0f75b24d368f
Italy Christian <60200749+ItalyChristian@users.noreply.github.com>
2021-02-12 15:50:38 -0300

    inserindo comentarios

--- calcular-mes-anterior-de-input/function mesAnterior().js
+++ calcular-mes-anterior-de-input/function mesAnterior().js
@@ -1,7 +1,10 @@
 function mesAnterior(){
 	
 	var pegaData =  $("#inserir-data").val(); 
+	// busca a data preenchida pelo usuario (ex: pegaData = "05/07/2021")
 	var buscaMes = pegaData.substring(3, 5);
+	//encontra na string os numeros referentes ao mês (ex: buscaMes = "07")
+	
 	
 	switch (buscaMes) {
 	    case "01":
@@ -14,13 +17,20 @@ function mesAnterior(){
 	    case "08":
 	    case "09":
 			var buscaDnv = buscaMes.substring(0);
+			// encontra o primeiro item (no caso o 0) e deixa só o segundo numero (ainda no nosso exemplo temos: buscaDnv = "7")
 			var subtraindo = buscaDnv - parseInt(1);
+			// subtrai do valor que extraimos (entao temos: substraindo = "6")
 			var arrumar2 = buscaMes.replace(buscaMes, ("0" + subtraindo));
+			//utilizamos a funcao replace para substituir os valores "07" (que extraimos no inicio) e substitumos por "0" + o resultado da subtraçao 
+			// ( entao: arrumar2 = "06" )
 			var finalData = pegaData.replace(pegaData.substring(3, 5), arrumar2);
+			// aqui nós buscamos de novo o a data inserida pelo usuário (no nosso exemplo: pegaData = "05/07/2021")
+			// e em seguida fazemos um novo recorte para substituir o "07" pelo resultado da nossa subtraçao (com o "0")
+			// entao ainda dentro do nosso exemplo , teriamos: finalData = "05/06/2021"
 	        break;
 
 	    case "10":
-	    	var subtraindo = buscaMes - parseInt(1);
+	    		var subtraindo = buscaMes - parseInt(1);
 			var arrumar2 = buscaMes.replace(buscaMes, ("0" + subtraindo));
 			var finalData = pegaData.replace(pegaData.substring(3, 5), arrumar2);
 	        break;
